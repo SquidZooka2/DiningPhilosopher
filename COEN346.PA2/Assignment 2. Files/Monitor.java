f131f3f19e32a1e18f8685c1b1ac191f515ca12b
Benjamin Gutman <benjamingutman@icloud.com>
2026-03-05 14:57:28 -0500

    Completed Task 2

--- COEN346.PA2/Assignment 2. Files/Monitor.java
+++ COEN346.PA2/Assignment 2. Files/Monitor.java
@@ -6,11 +6,17 @@
  */
 public class Monitor
 {
+	
 	/*
 	 * ------------
 	 * Data members
 	 * ------------
 	 */
+	
+	private int philosopherNumber;
+	private enum philosopherState{thinking, eating, talking, hungry};
+	private philosopherState[] State;
+	private boolean talking = false;
 
 
 	/**
@@ -19,6 +25,11 @@ public class Monitor
 	public Monitor(int piNumberOfPhilosophers)
 	{
 		// TODO: set appropriate number of chopsticks based on the # of philosophers
+		philosopherNumber = piNumberOfPhilosophers;
+		State = new philosopherState[philosopherNumber];
+		for (int i = 0; i <philosopherNumber; i++) {
+			State[i] = philosopherState.thinking;
+		}
 	}
 
 	/*
@@ -27,6 +38,29 @@ public class Monitor
 	 * You may need to add more procedures for task 5
 	 * -------------------------------
 	 */
+	
+	private int left(int i)
+	{
+	    return (i + philosopherNumber - 1) % philosopherNumber;
+	}
+
+	private int right(int i)
+	{
+	    return (i + 1) % philosopherNumber;
+	}
+	
+	private void test(int piTID) {
+			if (State[left(piTID)] != philosopherState.eating
+					&& State[right(piTID)] != philosopherState.eating
+					&& State[piTID] == philosopherState.hungry) {
+				State[piTID] = philosopherState.eating;
+		}
+	}
+	
+	private void test_talking(int piTID) {
+		if ()
+	}
+	
 
 	/**
 	 * Grants request (returns) to eat when both chopsticks/forks are available.
@@ -34,7 +68,18 @@ public class Monitor
 	 */
 	public synchronized void pickUp(final int piTID)
 	{
-		// ...
+		State[piTID] = philosopherState.hungry;
+		
+		test(piTID);
+		
+		if (State[piTID] != philosopherState.eating) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 	/**
@@ -43,7 +88,10 @@ public class Monitor
 	 */
 	public synchronized void putDown(final int piTID)
 	{
-		// ...
+		State[piTID] = philosopherState.thinking;
+		test(left(piTID));
+		test(right(piTID));
+		
 	}
 
 	/**
@@ -52,7 +100,15 @@ public class Monitor
 	 */
 	public synchronized void requestTalk(int id)
 	{
-		// ...
+		if (talking){
+			try {
+				wait();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		talking = true;
+		State[id] = philosopherState.talking;
 	}
 
 	/**
@@ -61,7 +117,9 @@ public class Monitor
 	 */
 	public synchronized void endTalk(int id)
 	{
-		// ...
+		State[id] = philosopherState.thinking;
+		talking = false;
+		notifyAll();
 	}
 }
 
